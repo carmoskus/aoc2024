b061342a220f427fa62544f247a3f0352fd319f7
Chris Armoskus <carmoskus@gmail.com>
2025-02-17 11:12:14 -0800

    Day2 finished, but it felt a bit brute forced

--- day2.py
+++ day2.py
@@ -11,15 +11,22 @@ in_txt = '''
 1 3 6 7 9
 '''.strip().split('\n')
 
+df1 = pd.DataFrame(
+    [[int(x) for x in y.split()] for y in in_txt],
+    columns = list(string.ascii_lowercase[:5])
+)
+df1
+
+df2 = df1.apply(lambda x: x.rolling(2).apply(lambda y: y.iat[1] - y.iat[0]), axis=1).dropna(axis=1)
+df2
+
 in_txt = [x.strip() for x in open('data/day2_input.txt').readlines() if x.strip() != ""]
 
 # Loop this
-def check_line(line: str):
-    in1 = pd.Series(int(x) for x in line.split())
-    in1
+in_series = [pd.Series([int(x) for x in line.split()]) for line in in_txt]
 
+def check_line(in1):
     in2 = in1.rolling(2).apply(lambda x: x.iloc[1] - x.iloc[0]).dropna()
-    in2
 
     # First check
     res1 = in2.lt(0).all() or in2.gt(0).all()
@@ -27,7 +34,22 @@ def check_line(line: str):
     res2 = in2.abs().max() <= 3
     return res1 and res2
 
-#
-sum(check_line(line) for line in in_txt)
+# Results for part1
+# [check_line(line) for line in in_series]
+sum(check_line(line) for line in in_series)
 
 # Part 2
+def check_line_vars(in1):
+    if check_line(in1):
+        return True
+    # Drop one input in series
+    for i in range(len(in1)):
+        if check_line(in1.drop(i)):
+            return True
+    
+    return False
+
+#
+# [check_line_vars(line) for line in in_series]
+
+sum(check_line_vars(line) for line in in_series)
