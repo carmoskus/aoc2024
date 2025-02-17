0b0503d0588683289f97f58852ec4d64424dfb18
Chris Armoskus <carmoskus@gmail.com>
2025-02-17 10:47:16 -0800

    Day2 part 1 works

--- day2.py
+++ day2.py
@@ -9,33 +9,25 @@ in_txt = '''
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
-'''.strip()
+'''.strip().split('\n')
 
-in_txt = open('data/day2_input.txt').read().strip()
+in_txt = [x.strip() for x in open('data/day2_input.txt').readlines() if x.strip() != ""]
 
+# Loop this
+def check_line(line: str):
+    in1 = pd.Series(int(x) for x in line.split())
+    in1
 
-in1 = pd.DataFrame([
-    [int(a) for a in b.split()]
-    for b in in_txt.split("\n")
-], columns = [x for x in string.ascii_lowercase[:5]])
-in1
+    in2 = in1.rolling(2).apply(lambda x: x.iloc[1] - x.iloc[0]).dropna()
+    in2
 
-in1.iloc[0, :].rolling(2).apply(lambda x: x[1] - x[0]).dropna()
-in1.iloc[1, :].rolling(2).apply(lambda x: x[1] - x[0]).dropna()
+    # First check
+    res1 = in2.lt(0).all() or in2.gt(0).all()
+    # Second check
+    res2 = in2.abs().max() <= 3
+    return res1 and res2
 
-in2 = in1.apply(lambda x: x.rolling(2).apply(lambda y: y[1] - y[0]), 1).drop('a', axis=1)
-in2
+#
+sum(check_line(line) for line in in_txt)
 
-# First check
-res1 = in2.lt(0).all(axis=1) | in2.gt(0).all(axis=1)
-res1
-
-# Second check
-# The remaining inputs
-in2[res1]
-
-in2[res1].abs().max(axis=1)
-res2 = in2[res1].abs().max(axis=1).le(3)
-res2
-
-res2.sum()
+# Part 2
