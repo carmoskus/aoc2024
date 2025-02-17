edd4a080386d6cc8ea228c14b686e075bc22dfbc
Chris Armoskus <carmoskus@gmail.com>
2025-02-17 13:51:14 -0800

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -46,3 +46,18 @@ def check_from(i: int, j: int, s: str):
 
 sum(check_from(i, j, 'XMAS')
     for i, j in itertools.product(range(in1.shape[0]), range(in1.shape[1])))
+
+# Part 2
+def check_ex(i: int, j: int):
+    if not check_letter(i, j, 'A'):
+        return 0
+    test1 = check_letter(i-1, j-1, 'M') and check_letter(i+1, j+1, 'S')
+    test2 = check_letter(i-1, j-1, 'S') and check_letter(i+1, j+1, 'M')
+    test3 = check_letter(i-1, j+1, 'M') and check_letter(i+1, j-1, 'S')
+    test4 = check_letter(i-1, j+1, 'S') and check_letter(i+1, j-1, 'M')
+    if (test1 or test2) and (test3 or test4):
+        return 1
+    return 0
+
+sum(check_ex(i, j)
+    for i, j in itertools.product(range(1, in1.shape[0]-1), range(1, in1.shape[1]-1)))
