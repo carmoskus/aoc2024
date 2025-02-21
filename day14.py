9f3f7aa60de46e3eb27ae50a28344a9034b3127e
Chris Armoskus <carmoskus@gmail.com>
2025-02-21 11:30:59 -0800

    Day14 part2

--- day14.py
+++ day14.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-import re
+import re, time
 
 in_re = r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)"
 
@@ -68,3 +68,55 @@ res3 = pd.Series(res2).value_counts()
 
 res3
 res3.drop(0).prod()
+
+#
+def safety_gen(my_in):
+    i = 0
+    while True:
+        res1 = [advance(x, i) for x in my_in]
+        res2 = [quad(x, y) for x, y in res1]
+        res3 = pd.Series(res2).value_counts()
+        yield res3.drop(0).prod()
+        i += 1
+
+g = safety_gen(in1)
+res4 = [next(g) for i in range(10000)]
+
+min(res4[:1000])
+res4.index(min(res4[:1000]))
+res4[369:374]
+
+min(res4)
+res4.index(min(res4))
+res4[1378:1384]
+
+min(res4[1400:])
+res4.index(min(res4[1400:]))
+res4[1480:1486]
+
+def mk_pic(my_in, n):
+    res = [advance(x, n) for x in my_in]
+    pic1 = pd.DataFrame([["." for i in range(nx)] for j in range(ny)])
+    for x, y in res:
+        c = pic1.iat[y, x]
+        if c == ".":
+            pic1.iat[y, x] = '1'
+        else:
+            pic1.iat[y, x] = str(int(pic1.iat[y, x])+1)
+    pic1.columns = ["" for i in range(pic1.shape[1])]
+    pic1.index = ["" for i in range(pic1.shape[0])]
+
+    return "\n".join("".join(pic1.iat[y, x] for x in range(pic1.shape[1])) for y in range(pic1.shape[0]))
+
+print(mk_pic(in1, 6532))
+
+res4[:100]
+res4[67:70]
+
+foo = pd.Series(res4)
+foo[foo < 120000000][:20]
+
+for i in foo[foo < 120000000].index[20:30]:
+    print(i)
+    print(mk_pic(in1, i))
+    time.sleep(0.4)
