7f3c1eb4f0cc0af4217918a4c3a760e290da1ef9
Chris Armoskus <carmoskus@gmail.com>
2025-02-22 09:48:12 -0800

    Can't figure out what's wrong with Day15 part2

--- day15.py
+++ day15.py
@@ -1,6 +1,121 @@
 
 import pandas as pd
 
+#
+def mv(r, c, dr, dc):
+    if df.iat[r, c] == "#":
+        # hit a wall
+        return
+    if df.iat[r, c] == ".":
+        # move into blank space
+        df.iat[r, c] = df.iat[r-dr, c-dc]
+        df.iat[r-dr, c-dc] = "."
+        return r, c
+    if df.iat[r, c] == "O":
+        # Pressing into a barrel
+        # See what moving into the next square does
+        beyond = mv(r+dr, c+dc, dr, dc)
+        if beyond is None:
+            # Hit a wall
+            return
+        # Then move myself
+        df.iat[r, c] = df.iat[r-dr, c-dc]
+        df.iat[r-dr, c-dc] = "."
+        return r, c
+    if df.iat[r, c] == "[":
+        if dc == 0:
+            # Moving vertically
+            b1 = mv(r+dr, c+dc, dr, dc)
+            if b1 is None:
+                return
+            else:
+                b2 = mv(r+dr, c+dc+1, dr, dc)
+                if b2 is None:
+                    # Hit wall on 2nd, reverse 1st
+                    df.iat[r, c] = df.iat[r+dr, c+dc]
+                    df.iat[r+dr, c+dc] = "."
+                    return
+                else:
+                    df.iat[r, c] = df.iat[r-dr, c-dc]
+                    df.iat[r-dr, c-dc] = "."
+                    return r, c
+        # Moving horizontally
+        b1 = mv(r+dr, c+dc, dr, dc)
+        if b1 is None:
+            return
+        df.iat[r, c] = df.iat[r-dr, c-dc]
+        df.iat[r-dr, c-dc] = "."
+        return r, c
+    if df.iat[r, c] == "]":
+        if dc == 0:
+            # Moving vertically
+            b1 = mv(r+dr, c+dc, dr, dc)
+            if b1 is None:
+                return
+            else:
+                b2 = mv(r+dr, c+dc-1, dr, dc)
+                if b2 is None:
+                    # Hit wall on 2nd, reverse 1st
+                    df.iat[r, c] = df.iat[r+dr, c+dc]
+                    df.iat[r+dr, c+dc] = "."
+                    return
+                else:
+                    df.iat[r, c] = df.iat[r-dr, c-dc]
+                    df.iat[r-dr, c-dc] = "."
+                    return r, c
+        # Moving horizontally
+        b1 = mv(r+dr, c+dc, dr, dc)
+        if b1 is None:
+            return
+        df.iat[r, c] = df.iat[r-dr, c-dc]
+        df.iat[r-dr, c-dc] = "."
+        return r, c
+
+def cmd(cmd):
+    # Find current loc
+    r = df.eq('@').idxmax(axis=1).idxmax()
+    c = df.eq('@').idxmax(axis=0).idxmax()
+    
+    dr = 0
+    dc = 0
+    if cmd == '<':
+        dc = -1
+    elif cmd == '>':
+        dc = 1
+    elif cmd == "^":
+        dr = -1
+    elif cmd == "v":
+        dr = 1
+    
+    if dr == 0 and dc == 0:
+        return
+
+    res = mv(r+dr, c+dc, dr, dc)
+    return res
+
+def str_to_map(map_str):
+    return pd.DataFrame([list(x) for x in map_str.split("\n")])
+
+def resize_map(m1):
+    m2 = ""
+    for r in range(m1.shape[0]):
+        for c in range(m1.shape[1]):
+            if m1.iat[r, c] == "#":
+                m2 += "##"
+            elif m1.iat[r, c] == ".":
+                m2 += ".."
+            elif m1.iat[r, c] == "O":
+                m2 += "[]"
+            elif m1.iat[r, c] == "@":
+                m2 += "@."
+        m2 += "\n"
+    return str_to_map(m2.strip())
+
+def check_map(m1):
+    l1 = m1.assign(r = lambda x: x.index).melt(id_vars='r', var_name='c')
+    return l1.value.value_counts(sort=False).sort_index()
+
+#
 in_txt = """
 ########
 #......#
@@ -25,6 +140,18 @@ in_txt = """
 ########
 
 <^^>>>vv<v>>v<<
+""".strip()
+
+in_txt = """
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^
 """.strip()
 
 in_txt = """
@@ -58,55 +185,12 @@ last_wall = in_txt.rfind("#")+1
 map_txt = in_txt[:last_wall].strip()
 cmd_txt = in_txt[last_wall:].strip()
 
-def mv(r, c, dr, dc):
-    if df.iat[r, c] == "#":
-        # hit a wall
-        return
-    if df.iat[r, c] == ".":
-        # move into blank space
-        df.iat[r, c] = df.iat[r-dr, c-dc]
-        df.iat[r-dr, c-dc] = "."
-        return r, c
-    if df.iat[r, c] == "O":
-        # Pressing into a barrel
-        # See what moving into the next square does
-        beyond = mv(r+dr, c+dc, dr, dc)
-        if beyond is None:
-            # Hit a wall
-            return
-        # Actually move the barrel
-        # df.iat[r+dr, c+dc] = df.iat[r, c]
-        # Then move myself
-        df.iat[r, c] = df.iat[r-dr, c-dc]
-        df.iat[r-dr, c-dc] = "."
-        return r, c
-def cmd(cmd):
-    # Find current loc
-    r = df.eq('@').idxmax(axis=1).idxmax()
-    c = df.eq('@').idxmax(axis=0).idxmax()
-    
-    dr = 0
-    dc = 0
-    if cmd == '<':
-        dc = -1
-    elif cmd == '>':
-        dc = 1
-    elif cmd == "^":
-        dr = -1
-    elif cmd == "v":
-        dr = 1
-    
-    if dr == 0 and dc == 0:
-        return
-
-    res = mv(r+dr, c+dc, dr, dc)
-    return res
-
 #
-df = pd.DataFrame([list(x) for x in map_txt.split("\n")])
+in1 = str_to_map(map_txt)
+df = in1.copy()
 df
 for k in cmd_txt:
-    cmd(k)
+    _ = cmd(k)
     # print(df)
 df
 
@@ -116,3 +200,26 @@ res1 = df.eq('O').assign(r=df.index).melt(
 res1
 
 res1.gps.sum()
+
+# Part 2
+in2 = resize_map(in1)
+
+df = in2.copy()
+df
+start_stats = check_map(df)
+for k in cmd_txt:
+    _ = cmd(k)
+    stats = check_map(df)
+    if not stats.equals(start_stats):
+        print("CHANGED")
+        break
+    # print(df)
+df
+
+res2 = df.eq('[').assign(r=df.index).melt(
+    id_vars='r', var_name='c').query('value').assign(gps=lambda x: x.c + 100 * x.r)
+res2
+
+res2.gps.sum()
+
+# Check stuff
